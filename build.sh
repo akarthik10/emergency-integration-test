35427dc76aff9899c30682c1c426fd6f1cfbc49e
Karthik A <akarthik101@gmail.com>
2017-04-05 22:54:49 -0400

    Xcode 8.2

--- build.sh
+++ build.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 cd umassemergency
 pod install
-xcodebuild -configuration Debug -workspace UMassEmergency.xcworkspace -scheme UMassEmergency -sdk iphonesimulator -destination platform="iOS Simulator",OS=10.1,name="iPhone 7 Plus" build test SYMROOT=$(PWD)/build
+xcodebuild -configuration Debug -workspace UMassEmergency.xcworkspace -scheme UMassEmergency -sdk iphonesimulator -destination platform="iOS Simulator",OS=10.1,name="Custom iPhone 7 Plus" build test SYMROOT=$(PWD)/build
