67906a8eda8bcdde8ed7f0c2d0310b4ef5b7b6d4
Karthik A <akarthik101@gmail.com>
2017-04-05 21:47:58 -0400

    Pod update and device ID change

--- build.sh
+++ build.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 cd umassemergency
-pod init
-xcodebuild -configuration Debug -workspace UMassEmergency.xcworkspace -scheme UMassEmergency -sdk iphonesimulator -destination platform="iOS Simulator",OS=10.2,id="7B6F8C6B-B67A-4F64-BB70-AE1FF077ACC2" build test SYMROOT=$(PWD)/build
+pod install
+xcodebuild -configuration Debug -workspace UMassEmergency.xcworkspace -scheme UMassEmergency -sdk iphonesimulator -destination platform="iOS Simulator",OS=10.2,id="86B2CA7C-0247-4257-BFEA-8035084AF2C" build test SYMROOT=$(PWD)/build
