815c25ad77439c5c1132ad05eb9dcbc0ce87c1c9
Karthik A <akarthik101@gmail.com>
2017-04-07 23:16:10 -0400

    add python and selenium test

--- build.sh
+++ build.sh
@@ -1,4 +1,9 @@
 #!/usr/bin/env bash
+
+pip install selenium
+
+python test.py &
+
 cd umassemergency
 pod install
 xcodebuild -configuration Debug -workspace UMassEmergency.xcworkspace -scheme UMassEmergency -sdk iphonesimulator -destination platform="iOS Simulator",OS=10.2,name="iPhone 7 Plus" build test SYMROOT=$(PWD)/build
