177bfdc6c7534412ebcd5fb5d6e9d4dceda160cd
Karthik A <akarthik101@gmail.com>
2017-04-09 02:02:15 -0400

    Logger init

--- build.sh
+++ build.sh
@@ -9,4 +9,6 @@ cd ..
 touch iosoutput.log
 ./selenium.sh &
 
+./logger.sh &
+
 xcodebuild -configuration Debug -workspace umassemergency/UMassEmergency.xcworkspace -scheme UMassEmergency -sdk iphonesimulator -destination platform="iOS Simulator",OS=10.2,name="iPhone 7 Plus" build test SYMROOT=$(PWD)/build | tee iosoutput.log
