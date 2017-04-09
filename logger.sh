b10e7bbce031c725e70f04aa7dca781c6d2a4f61
Karthik A <akarthik101@gmail.com>
2017-04-09 02:11:44 -0400

    Added logger update

--- logger.sh
+++ logger.sh
@@ -2,7 +2,7 @@
 
 
 
-while [ ! -f tail -f ~/Library/Logs/CoreSimulator/86B2CA7C-0247-4257-BFEA-8035084AF2CF/system.log ]
+while [ ! -f ~/Library/Logs/CoreSimulator/86B2CA7C-0247-4257-BFEA-8035084AF2CF/system.log ]
 do
   sleep 2
 done
