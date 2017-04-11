4c4556c6a8239b5cfa2c7234c633618c7474e6b3
Karthik A <akarthik101@gmail.com>
2017-04-10 23:22:43 -0400

    Retry logic update

--- build.sh
+++ build.sh
@@ -11,11 +11,11 @@ cd ..
    until [ $n -ge 5 ]
    do
       unbuffer xcodebuild -configuration Debug -workspace umassemergency/UMassEmergency.xcworkspace -scheme UMassEmergency -sdk iphonesimulator -destination platform="iOS Simulator",OS=10.2,name="iPhone 7 Plus" build test SYMROOT=$(PWD)/build | tee iosoutput.log | xcpretty && break  # substitute your command here
-      if [ ${PIPESTATUS[0]} -eq 0 ]; then
+      if [[ ${pipestatus[0]} -eq 0 ]]; then
       	exit 0
       fi
       n=$[$n+1]
-      echo "Retrying again.."
+      echo "Retrying again.. $n "
       killall -9 Simulator
       killall -9 com.apple.CoreSimulator.CoreSimulatorService
       sleep 15
