643e1a6bf5e5175e80b1867a86a2e1457672e4d6
Karthik A <akarthik101@gmail.com>
2017-04-10 22:09:04 -0400

    Update travis xcpretty set

--- build.sh
+++ build.sh
@@ -6,4 +6,4 @@ cd umassemergency
 pod install
 cd ..
 
-unbuffer xcodebuild -configuration Debug -workspace umassemergency/UMassEmergency.xcworkspace -scheme UMassEmergency -sdk iphonesimulator -destination platform="iOS Simulator",OS=10.2,name="iPhone 7 Plus" build test SYMROOT=$(PWD)/build | tee iosoutput.log
+unbuffer xcodebuild -configuration Debug -workspace umassemergency/UMassEmergency.xcworkspace -scheme UMassEmergency -sdk iphonesimulator -destination platform="iOS Simulator",OS=10.2,name="iPhone 7 Plus" build test SYMROOT=$(PWD)/build | tee iosoutput.log | xcpretty && exit ${PIPESTATUS[0]}
