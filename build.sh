88c8744c9307663d5ed90e9af21b88d214713de3
Karthik A <akarthik101@gmail.com>
2017-04-10 23:00:31 -0400

    Retry logic

--- build.sh
+++ build.sh
@@ -6,4 +6,22 @@ cd umassemergency
 pod install
 cd ..
 
-unbuffer xcodebuild -configuration Debug -workspace umassemergency/UMassEmergency.xcworkspace -scheme UMassEmergency -sdk iphonesimulator -destination platform="iOS Simulator",OS=10.2,name="iPhone 7 Plus" build test SYMROOT=$(PWD)/build | tee iosoutput.log | xcpretty && exit ${PIPESTATUS[0]}
+
+   n=0
+   until [ $n -ge 5 ]
+   do
+      unbuffer xcodebuild -configuration Debug -workspace umassemergency/UMassEmergency.xcworkspace -scheme UMassEmergency -sdk iphonesimulator -destination platform="iOS Simulator",OS=10.2,name="iPhone 7 Plus" build test SYMROOT=$(PWD)/build | tee iosoutput.log | xcpretty && break  # substitute your command here
+      if [ ${PIPESTATUS[0]} -eq 0 ]; then
+      	exit 0
+      fi
+      n=$[$n+1]
+      echo "Retrying again.."
+      killall -9 Simulator
+      killall -9 com.apple.CoreSimulator.CoreSimulatorService
+      sleep 15
+
+   done
+   echo "Retry failed, exiting.."
+   exit 1
+
+
