07368b0ac8ec3f6b48e90521c8a22b24ff0f819b
Karthik A <akarthik101@gmail.com>
2017-04-10 20:26:46 -0400

    Added unbuffer package

--- build.sh
+++ build.sh
@@ -6,4 +6,4 @@ cd umassemergency
 pod install
 cd ..
 
-xcodebuild -configuration Debug -workspace umassemergency/UMassEmergency.xcworkspace -scheme UMassEmergency -sdk iphonesimulator -destination platform="iOS Simulator",OS=10.2,name="iPhone 7 Plus" build test SYMROOT=$(PWD)/build | tee iosoutput.log | xcpretty -t
+unbuffer xcodebuild -configuration Debug -workspace umassemergency/UMassEmergency.xcworkspace -scheme UMassEmergency -sdk iphonesimulator -destination platform="iOS Simulator",OS=10.2,name="iPhone 7 Plus" build test SYMROOT=$(PWD)/build | tee iosoutput.log | xcpretty -t
