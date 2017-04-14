27b95a4fd4e44d2589fe55d4285d77a35d669058
Karthik A <akarthik101@gmail.com>
2017-04-14 11:29:27 -0400

    Update promotion

--- promote.sh
+++ promote.sh
@@ -1,3 +1,5 @@
 #!/usr/bin/env bash
 
-cd em_portal; { echo \"\$admin = new \App\Models\Role(); \$admin->name = 'admin'; \$admin->save(); \$admin = \App\Models\Role::where('name','=','admin')->first(); \$user = \App\Models\User::where('email','=','"+user+"')->first(); \$user->attachRole(\$admin); exit;\" ; cat ; } | php artisan tinker; cd ..; 
+cd em_portal 
+{ echo '$admin = new \App\Models\Role(); $admin->name = "admin"; $admin->save(); $admin = \App\Models\Role::where("name","=","admin")->first(); $user = \App\Models\User::where("email","=","'"$0"'")->first(); $user->attachRole($admin); exit;' ; cat ; } | php artisan tinker
+cd ..
