d8cb227475ea8d12f71e45082824e696c5bae6ce
Karthik A <akarthik101@gmail.com>
2017-04-16 16:55:08 -0400

    Display build status

--- build.sh
+++ build.sh
@@ -11,6 +11,7 @@ cd ..
    until [ $n -ge 5 ]
    do
       env NSUnbufferedIO=YES xcodebuild -configuration Debug -workspace umassemergency/UMassEmergency.xcworkspace -scheme UMassEmergency -sdk iphonesimulator -destination platform="iOS Simulator",OS=10.2,name="iPhone 7 Plus" build test SYMROOT=$(PWD)/build | tee iosoutput.log | xcpretty
+      echo "Exit status is $?"
       if grep -q "TEST FAILED" "iosoutput.log"; then
          echo "Exiting with success"
       	exit 0
