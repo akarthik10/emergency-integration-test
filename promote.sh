dac696c55eb5467cd3980b6b71450998d86fd2dd
Karthik A <akarthik101@gmail.com>
2017-05-16 07:34:51 +0530

    Slight delay before promoting user

--- promote.sh
+++ promote.sh
@@ -1,6 +1,5 @@
 #!/usr/bin/env bash
 
 cd em_portal 
-{ echo '$admin = new \App\Models\Role(); $admin->name = "admin"; $admin->save();' ; cat ; } | php artisan tinker
-{ echo '$admin = \App\Models\Role::where("name","=","admin")->first(); $user = \App\Models\User::where("email","=","'"$1"'")->first(); $user->attachRole($admin); exit;' ; cat ; } | php artisan tinker
+{ echo '$admin = \App\Models\Role::where("name","=","admin")->first(); if (!$admin) { $admin = new \App\Models\Role(); $admin->name = "admin"; $admin->save(); }; $user = \App\Models\User::where("email","=","'"$1"'")->first(); $user->attachRole($admin); exit;' ; cat ; } | php artisan tinker
 cd ..
